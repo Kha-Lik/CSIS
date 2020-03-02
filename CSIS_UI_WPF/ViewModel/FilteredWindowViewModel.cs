a95816d5936349be9323d4058f2643924f268ad8
MykolaV <kolya.vasevich@gmail.com>
2020-03-02 10:11:46 +0200

    removed needless feature

--- CSIS_UI_WPF/ViewModel/FilteredWindowViewModel.cs
+++ CSIS_UI_WPF/ViewModel/FilteredWindowViewModel.cs
@@ -11,11 +11,6 @@ namespace CSIS_UI_WPF.ViewModel
 
         public ObservableCollection<Cosmetic> Filtered => _filter.Filtered;
 
-        private RelayCommand _setMaxAmountToOrderCommand;
-
-        public RelayCommand SetMaxAmountToOrderCommand =>
-            _setMaxAmountToOrderCommand ??= new RelayCommand(obj => SetMaxAmountToOrder(int.Parse(obj.ToString())));
-
         public FilteredWindowViewModel(Storage storage) => _filter = new FilterFacade(storage);
 
         public void SetMaxAmountToOrder(int amount) => _filter.SetMaxAmountToOrder(amount);
