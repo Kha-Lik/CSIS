e558c12cd6d811c7a882a70de38e59d4e38c5de5
MykolaV <kolya.vasevich@gmail.com>
2020-04-19 20:37:50 +0300

    created dependency installer for BLL.Impl

---
+++ BLL.Impl/BllInstaller.cs
@@ -0,0 +1,18 @@
+﻿using BLL.Abstract;
+using BLL.Mapper;
+using BLL.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BLL
+{
+    public static class BllInstaller
+    {
+        public static void InstallBll(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<ICosmeticService, CosmeticService>()
+                .AddSingleton<IFilterService, FilterService>()
+                .AddSingleton<IFacade, Facade>()
+                .BindMapper();
+        }
+    }
+}
