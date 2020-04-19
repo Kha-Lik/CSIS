ffecc8c04fac203582909e8a02b36f87677148c6
MykolaV <kolya.vasevich@gmail.com>
2020-04-19 20:37:40 +0300

    created dependency installer for DAL.Impl

---
+++ DAL.Impl/DalInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using DAL.Abstract;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DAL.Impl
+{
+    public static class DalInstaller
+    {
+        public static void InstallDal(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<CsisDbContext>(x =>
+                    x.UseSqlServer(ConfigurationManager.ConnectionStrings["Csis"].ConnectionString))
+                .AddSingleton<IUnitOfWork, UnitOfWork>()
+                .AddSingleton<ICosmeticRepository, CosmeticRepository>();
+        }
+        
+    }
+}
